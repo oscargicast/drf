5ec821a7c7b3aea296acfe123f5c410b04c71f6f
Oscar Giraldo Castillo <oscar.gi.cast@gmail.com>
2014-08-16 01:46:07 -0500

    add django rest framework Request and Response objects

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, url
+from rest_framework.urlpatterns import format_suffix_patterns
 
 
 urlpatterns = patterns(
@@ -6,3 +7,5 @@ urlpatterns = patterns(
     url(r'^snippets/$', 'snippet_list'),
     url(r'^snippets/(?P<pk>[0-9]+)/$', 'snippet_detail'),
 )
+
+urlpatterns = format_suffix_patterns(urlpatterns)
